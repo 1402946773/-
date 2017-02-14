b979e78fda1cae49e2bea34a0cd6479cf7dac24f
zhaoBo <1402946773@qq.com>
2017-02-14 21:27:32 +0800

    sub

--- a.js
+++ a.js
@@ -2,8 +2,9 @@
 * @Author: Marte
 * @Date:   2017-02-14 21:21:05
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-02-14 21:24:42
+* @Last Modified time: 2017-02-14 21:27:17
 */
 
 'use strict';
-这是新添加的 东西
+哈哈
+当你
